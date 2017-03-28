321cd752b05c241597f8082edd749a6b553147b2
Nic Sandfield <niczero@wow.com>
2017-03-28 19:22:12 +0100

    Add UserAgent extensions
      - cookies are loaded upon creation, saved upon destruction
      - the useragent string is set and used
      - the referer is stored, but not yet used

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mojar::UserAgent
 
+```perl
+  my $ua = Mojar::UserAgent->new(
+    cookiejar_path => 'cookies.txt',
+    referer => 'https://example.com/products'
+  )->name('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Firefox/89.0')
+```
--- lib/Mojar/UserAgent.pm
+++ lib/Mojar/UserAgent.pm
@@ -1,7 +1,50 @@
 package Mojar::UserAgent;
 use Mojo::UserAgent -base;
 
-our $VERSION = 0.011;
+our $VERSION = 0.012;
+
+my $default_name = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0';
+
+sub new {
+  my ($proto, %param) = @_;
+  my $self = $proto->SUPER::new(%param);
+  $self->name($param{name} // $default_name);
+  $self->load_cookies;
+  return $self;
+}
+
+has on_error => sub {
+  sub {
+    my ($ua, $loop, $msg) = @_;
+    $ua->log->error($msg);
+  }
+};
+
+has cookiejar_path => 'data/cookies.txt';
+
+has 'referer';
+
+sub name {
+  my $self = shift;
+  return $self->transactor->name unless @_;
+
+  $self->transactor->name(shift);
+  return $self;
+}
+
+sub load_cookies {
+  my ($self, $filename) = @_;
+  $filename //= $self->cookiejar_path;
+
+  my $jar = $self->cookie_jar->with_roles('+Persistent')->file($filename);
+  $jar->load if -f $filename;
+
+  return $self;
+}
+
+DESTROY {
+  $_[0]->cookie_jar->save;
+}
 
 1;
 __END__
@@ -9,6 +52,13 @@ __END__
 =head1 NAME
 
 Mojar::UserAgent - A modest extension of the wonderful Mojo::UserAgent.
+
+=head1 SYNOPSIS
+
+  my $ua = Mojar::UserAgent->new(
+    cookiejar_path => 'cookies.txt',
+    referer => 'https://example.com/products'
+  )->name('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Firefox/89.0')
 
 =head1 DESCRIPTION
 
